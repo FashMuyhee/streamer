bff89d52cf334e8eb6d8a071e6ad5c038c2b6fd0
Fash Muyhee <fashmuyidavid@gmail.com>
2024-04-11 11:57:30 +0100

    clean up

--- src/views/onboarding.tsx
+++ src/views/onboarding.tsx
@@ -1,16 +1,19 @@
-import {View, Text} from 'react-native';
 import React from 'react';
-import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {ScreenParams} from '@routes/type';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { ScreenParams } from '@routes/type';
+import { useTheme } from '@hooks';
+import { ScreenWrapper, Text } from '@components';
 
 type Props = {
   navigation: NativeStackNavigationProp<ScreenParams>;
 };
 
-export const OnboardingScreen = ({navigation}: Props) => {
+export const OnboardingScreen = ({ navigation }: Props) => {
+  const { theme } = useTheme();
+
   return (
-    <View>
-      <Text>Onboarding</Text>
-    </View>
+    <ScreenWrapper>
+      <Text fontSize={30}>Whereas disregard and contempt for human rights have resulted</Text>
+    </ScreenWrapper>
   );
 };
