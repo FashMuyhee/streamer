f317a1fc6832c8cf4fd13a34bbc12f0b671d778d
Fash Muyhee <fashmuyidavid@gmail.com>
2024-04-11 19:37:28 +0100

    add notification permission

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -2,18 +2,29 @@ import React from 'react';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { ScreenParams } from '@routes/type';
 import { Button, ScreenWrapper, Text } from '@components';
+import notifee from '@notifee/react-native';
 
 type Props = {
   navigation: NativeStackNavigationProp<ScreenParams>;
 };
 
 export const HomeScreen = ({ navigation }: Props) => {
+  async function requestUserPermission() {
+    const settings = await notifee.requestPermission();
+  }
+
+  React.useEffect(() => {
+    requestUserPermission();
+  }, []);
+
   return (
     <ScreenWrapper noEdges style={{ alignItems: 'center', justifyContent: 'center' }}>
-      <Text fontSize={35}>Welcome To Streamer</Text>
-      <Button title="Join Stream" />
+      <Text fontSize={35} textAlign="center">
+        Welcome To Streamer
+      </Text>
+      <Button title="Join Stream" onPress={() => navigation.navigate('call', { host: false })} />
       <Text>Or </Text>
-      <Button title="Create Stream" onPress={() => navigation.navigate('call')} />
+      <Button title="Create Stream" onPress={() => navigation.navigate('call', { host: true })} />
     </ScreenWrapper>
   );
 };
