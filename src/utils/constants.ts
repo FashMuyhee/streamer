4e2c46a8c6b7221c5e72344ecebf0b9ee7f5b231
Fash Muyhee <fashmuyidavid@gmail.com>
2024-04-11 12:04:59 +0100

    add border radius constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -44,6 +44,8 @@ export const SCREEN_PADDING = 24;
 export const SCREEN_HEIGHT = Dimensions.get('window').height;
 export const SCREEN_WIDTH = Dimensions.get('window').width;
 
+export const BORDER_RADIUS = 7;
+
 export const KEYBOARD_BEHAVIOR = 'padding';
 
 /**
