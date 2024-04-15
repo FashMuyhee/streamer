0fe99864c97912775f398f48d06b5d94b525f394
Fash Muyhee <fashmuyidavid@gmail.com>
2024-04-15 12:28:03 +0100

    fix animation

--- src/views/home/components/Fab.tsx
+++ src/views/home/components/Fab.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Animated, Easing, View } from 'react-native';
 import React from 'react';
-import { COLORS, IS_ANDROID, SHADOW_STYLE } from '@utils';
+import { COLORS, IS_ANDROID, SCREEN_PADDING, SHADOW_STYLE } from '@utils';
 import { AddIcon, ArrowRightIcon, StreamIcon } from '@components';
 import { IconButton } from '@components/commons/IconButton';
 
@@ -12,17 +12,18 @@ export const Fab = (props: Props) => {
   const scaleAnimate = React.useRef(new Animated.Value(SIZE)).current;
   const [isToggle, setIsToggle] = React.useState(false);
 
-  const animateElement = () => {
-    const toValue = isToggle ? SIZE * 3 + 10 : SIZE;
+  const onToggleAnimation = React.useCallback(() => {
+    const flipped = !isToggle;
+    const toValue = flipped ? SIZE * 3 + 10 : SIZE;
     Animated.timing(scaleAnimate, {
-      toValue: toValue,
+      toValue,
       duration: 500,
       easing: Easing.ease,
       useNativeDriver: false,
     }).start(() => {
-      setIsToggle(!isToggle);
+      setIsToggle(flipped);
     });
-  };
+  }, [isToggle]);
 
   const animationStyle = {
     width: scaleAnimate,
@@ -30,10 +31,10 @@ export const Fab = (props: Props) => {
 
   return (
     <Animated.View style={[styles.fabContainer, animationStyle]}>
-      <IconButton icon={isToggle ? <StreamIcon /> : <ArrowRightIcon />} onPress={animateElement} size={SIZE} />
+      <IconButton icon={isToggle ? <ArrowRightIcon /> : <StreamIcon />} onPress={onToggleAnimation} size={SIZE} />
       <View style={{ alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row' }}>
-        <IconButton icon={<StreamIcon />} onPress={animateElement} size={SIZE} />
-        <IconButton icon={<AddIcon />} onPress={animateElement} size={SIZE} />
+        <IconButton icon={<StreamIcon />} onPress={() => {}} size={SIZE} />
+        <IconButton icon={<AddIcon />} onPress={() => {}} size={SIZE} />
       </View>
     </Animated.View>
   );
@@ -43,6 +44,7 @@ const styles = StyleSheet.create({
   fabContainer: {
     position: 'absolute',
     bottom: IS_ANDROID ? 20 : 40,
+    right: SCREEN_PADDING,
     alignSelf: 'flex-end',
     height: SIZE,
     borderRadius: SIZE / 2,
@@ -50,6 +52,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     flexDirection: 'row',
-    ...SHADOW_STYLE,
   },
 });
