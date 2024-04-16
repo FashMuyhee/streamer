7aa7d35a085feeefeb67edfc60a1e8bcd39049c3
Fash Muyhee <fashmuyidavid@gmail.com>
2024-04-16 23:36:47 +0100

    add login hook

--- src/views/auths/api/hooks.tsx
+++ src/views/auths/api/hooks.tsx
@@ -1,4 +1,4 @@
-import { AuthError, FirebaseAuthErrorCode, RegisterPayload } from './types';
+import { AuthError, FirebaseAuthErrorCode, LoginPayload, RegisterPayload } from './types';
 import usePostRequest from '@hooks/usePostRequest';
 import auth from '@react-native-firebase/auth';
 import Toast from 'react-native-toast-message';
@@ -35,6 +35,35 @@ export const useRegister = () => {
     },
     onSuccess: () => {
       Toast.show({ text1: 'Registration Successful', type: 'success' });
+    },
+  });
+};
+
+export const useLogin = () => {
+  return usePostRequest<any, LoginPayload, AuthError>({
+    onPost: (payload) => {
+      const login = async () => {
+        const res = await auth().signInWithEmailAndPassword(payload.email, payload.password);
+      };
+      return login();
+    },
+    onError: (error) => {
+      switch (error.code) {
+        case FirebaseAuthErrorCode.INVALID_EMAIL:
+          Toast.show({ text1: 'Invalid Email Address', type: 'error' });
+          break;
+        case FirebaseAuthErrorCode.USER_DISABLED:
+          Toast.show({ text1: 'Account disabled contact admin', type: 'error' });
+          break;
+        case FirebaseAuthErrorCode.USER_NOT_FOUND:
+          Toast.show({ text1: 'User not found', type: 'error' });
+          break;
+        case FirebaseAuthErrorCode.WEAK_PASSWORD:
+          Toast.show({ text1: 'Weak Password, Please use a stronger password', type: 'error' });
+        default:
+          Toast.show({ text1: 'Something went wrong please try again', type: 'error' });
+          break;
+      }
     },
   });
 };
