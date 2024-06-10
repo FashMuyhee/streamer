4a7eb079e7cd0da4d61a0632a5c2ed2e1796f3b0
Fash Muyhee <fashmuyidavid@gmail.com>
2024-06-10 19:24:32 +0100

    clean up

--- src/views/call/hooks/useEndStream.ts
+++ src/views/call/hooks/useEndStream.ts
@@ -5,13 +5,10 @@ import { onFindStreamById } from '@views/home/firebase';
 export const useEndStream = (streamId: string) => {
   const onEndStream = async () => {
     const stream = await onFindStreamById(streamId);
-    console.log('🚀 ~ onEndStream ~ stream:', stream);
     if (stream) {
       const dbRef = fireDb().collection(FIRESTORE_COLLECTIONS.STREAMS).doc(streamId);
       await dbRef.update({ endedAt: new Date().toString() });
-    } else {
-      console.log('sdkfmdknf');
-    }
+    } 
   };
 
   return { onEndStream };
